69d8aa07d5cff142861e968aa4b5f04f4cc1f0a7
jide solanke <jidedorcas@gmail.com>
2018-12-06 21:49:52 +0100

    maven update

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,11 +2,16 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+
+		ApplicationContext ctx  =SpringApplication.run(Spring5RecipeAppApplication.class, args);
+		System.out.print("*****************Beans*************************");
+
+
 	}
 }
